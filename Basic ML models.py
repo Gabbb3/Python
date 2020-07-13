9acec8e925ffa75daf4d6d2b8ef7f3e339158db0
Gabbb3 <Lzhgab@gmail.com>
2020-07-13 17:27:32 +0800

    Update Basic ML models.py

--- Basic ML models.py
+++ Basic ML models.py
@@ -1,5 +1,6 @@
-# TO DO: ADD XGBOOST, LSTM, SVM, Logistic Regression
 # k-means to do discretization
+# New file: Oversampling methods SMOTE, ADASYN
+# Other file: PDPBox, P&C and left_join back
 ######################### Splitting dataframe for modelling #########################
 Y = df["y"]
 X = df.drop(["y"], axis=1)
@@ -81,6 +82,153 @@ ADA_cv = GridSearchCV(ADA, param_grid=param_grid, cv=5, scoring = "accuracy")
 ADA_cv = ADA_cv.fit(X_train, Y_train)
 pred_ADA = ADA.predict(X_test)
 
+##################################### Logistic Regression Classifier ########################################
+
+from sklearn.linear_model import LogisticRegression
+
+# Removing low variance inputs due to initial error from fitting logistic model
+from sklearn.feature_selection import VarianceThreshold
+
+def variance_threshold_selector(data, threshold=0.5):
+    # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.VarianceThreshold.html
+    # https://stackoverflow.com/a/39813304/1956309
+    selector = VarianceThreshold(threshold)
+    selector.fit(data)
+    return data[data.columns[selector.get_support(indices=True)]]
+# min_variance = .9 * (1 - .9)  # You can play here with different values.
+min_variance = 0.01
+low_variance_removed = variance_threshold_selector(X_train, min_variance) 
+print('columns removed:', low_variance_removed.columns)
+X_train_var = low_variance_removed
+
+param_grid={"C":np.logspace(-3,3,7), 
+            "penalty":["l1, l2"], 
+            "max_iter": [1000, 2000]}
+
+#### Option 1. Using sklearn ####
+LR_cv = GridSearchCV(LogisticRegression(),grid,cv=5)
+LR_cv = LR_cv.fit(X_train_var,Y_train) ## Initial ERROR: Check levels of inputs - Likely to have 99%~ in one level for an input
+
+# View best hyperparameters
+print('Best C:', LR_cv.best_estimator_.get_params())
+
+# Using abs(coef) to get feature importance
+feat_labels = X_train_var.columns.values
+importances = LR_cv.best_estimator_.coef_[0]
+indices = np.argsort(importances)
+lr_importance = pd.DataFrame()
+lr_importance["features"] = feat_labels
+lr_importance["importances"] = importances
+lr_importance = lr_importance.sort_values(["importances"], ascending=0)
+
+for i in range(len(lr_importance["importances"])):
+    if lr_importance["importances"][i] < 0:
+        lr_importance["importances"][i] = -lr_importance["importances"][i] # Changing values of lasso coeff to absolute values
+
+lr_importance = lr_importance.sort_values(["importances"], ascending=0)
+lr_importance
+
+# Remember to remove the low variance columns 
+X_test_var = X_test[low_variance_removed.columns]
+
+#### Option 2. Using statsmodel ####
+import statsmodels.api as sm
+from statsmodels.formula.api import logit
+
+# Describe model
+mod = sm.Logit(Y_train.astype(float), X_train_var.astype(float))    
+
+# Fit model
+clf_logit = mod.fit(maxiter=3000)      
+# Summarize model
+print(clf_logit.summary())  
+print(clf_logit.get_margeff().summary())
+
+# Feature importance - Remove pvalue >=0.05 and sort by coef
+df_logit = pd.DataFrame([X_test_var.columns.values, clf_logit.params, clg_logit.pvalues]).T
+df_logit.columns = ["features", "coef", "pvalues"]
+df_logit["pvalues"] = pd.to_numeric(df_logit["pvalues"])
+df_logit["coef"] = pd.to_numeric(df_logit["coef"])
+df_logit = df_logit.drop(df_logit[df_logit.pvalues > 0.05].index)
+df_logit = df_logit.dropna()
+logit_importance = df_logit.sort_values(["coef"], ascending=0)
+logit_importance
+
+# ROC Curve & AUC Score - If used often, make it into a function instead
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+
+pred_logit = clf_logit.predict(X_test_var)
+logit_auc = roc_auc_score(Y_test, pred_logit)
+ns_probs = [0 for _ in range(len(Y_test))]
+ns_auc = roc_auc_score(Y_test, ns_probs)
+Y_test_array = np.array(Y_test)
+Y_test_array = Y_test_array.astype(float)
+# summarize scores
+print('No Skill: ROC AUC=%.3f' % (ns_auc))
+print('Logistic: ROC AUC=%.3f' % (logit_auc))
+# calculate roc curves
+ns_fpr, ns_tpr, _ = roc_curve(Y_test_array, ns_probs)
+logit_fpr, logit_tpr, _ = roc_curve(Y_test_array, pred_logit)
+# plot the roc curve for the model
+plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+plt.plot(logit_fpr, logit_tpr, marker='.', label='Logistic')
+# axis labels
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+# show the legend
+plt.legend()
+# show the plot
+plt.show()
+
+##################################### Support Vector Machine (SVM) Classifier ########################################
+
+kernels = ['Polynomial', 'RBF', 'Sigmoid','Linear']
+
+def getClassifier(ktype):
+    if ktype == 0:
+        # Polynomial kernal
+        return SVC(kernel='poly', degree=8, gamma="auto")
+    elif ktype == 1:
+        # Radial Basis Function kernal
+        return SVC(kernel='rbf', gamma="auto")
+    elif ktype == 2:
+        # Sigmoid kernal
+        return SVC(kernel='sigmoid', gamma="auto")
+    elif ktype == 3:
+        # Linear kernal
+        return SVC(kernel='linear', gamma="auto")
+
+for i in range(4):
+# Train a SVC model using different kernal
+    svclassifier = getClassifier(i) 
+    svclassifier.fit(X_train, Y_train)
+# Make prediction
+    y_pred = svclassifier.predict(X_test)
+# Evaluate our model
+    print("Evaluation:", kernels[i], "kernel")
+    print(classification_report(Y_test,y_pred))
+
+# Using the best kernel (in this case, using linear)
+clf_sv = getClassifier(3) 
+clf_sv.fit(X_train, Y_train)
+feat_labels = X_train.columns.values
+importances = clf_sv.coef_.T
+
+# Feature importance
+indices = np.argsort(importances)
+svc_importance = pd.DataFrame()
+svc_importance["features"] = feat_labels
+svc_importance["importances"] = importances
+svc_importance = svc_importance.sort_values(["importances"], ascending=0)
+
+for i in range(len(svc_importance["importances"])):
+    if svc_importance["importances"][i] < 0:
+        svc_importance["importances"][i] = -svc_importance["importances"][i] # Changing values of svc coeff to absolute values
+
+svc_importance = svc_importance.sort_values(by=['importances'], ascending=False)
+svc_importance
+
 ##################################### Gradient Boosting Classifier ########################################
 
 from sklearn.ensemble import GradientBoostingClassifier
@@ -101,6 +249,52 @@ gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_ # To print gr
 # 2) min_samples_leaf - param_grid3 = {'min_samples_leaf':range(30,71,10)} #50-100
 # 3) max_features - param_grid4 = {'max_features':range(7,20,2)} #sqrt, auto, log2
 # 4) subsample - param_grid5 = {'subsample':[0.6,0.7,0.75,0.8,0.85,0.9]}
+
+##################################### XGBoost Classifier ########################################
+import xgboost as xgb
+from xgboost.sklearn import XGBClassifier
+
+param_grid = {
+ 'max_depth':range(3,10,2),
+ 'min_child_weight':range(1,6,2)
+}
+
+# Param training can be done similar to the above Gradient Boosting Classifer instead of below
+xg_cv = GridSearchCV(estimator = XGBClassifier(learning_rate =0.1, n_estimators=140, max_depth=5,
+ min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27), 
+ param_grid = param_grid, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+
+xg_cv.fit(X_train,Y_train)
+
+# xg.cv_results_
+xg_cv.best_params_, xg_cv.best_score_
+
+# Using above max_depth and min_child_weight to tune gamma
+param_grid2 = {
+ 'gamma':[i/10.0 for i in range(0,5)]
+}
+
+xg_cv2 = GridSearchCV(estimator = XGBClassifier(learning_rate =0.1, n_estimators=140, max_depth=9,
+ min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_grid2, scoring='roc_auc',n_jobs=4, cv=5)
+
+xg_cv2.fit(X_train,Y_train)
+xg_cv2.best_params_, xg_cv2.best_score_
+
+# Prediction
+pred_xg = xg_cv2.predict(X_test)
+
+# Feature importance
+feat_labels = X_test.columns.values
+importances = xg_cv2.best_estimator_.feature_importances_
+indices = np.argsort(importances)
+xg_importance = pd.DataFrame()
+xg_importance["features"] = feat_labels
+xg_importance["importances"] = importances
+xg_importance = xg_importance.sort_values(["importances"], ascending=0)
+xg_importance
 
 ##################################### Neural Network Classifier (keras) ########################################
 
@@ -130,6 +324,70 @@ clf_vc = VotingClassifier(estimators=[('DT', DT_cv), ("NB", NB_cv), ("RF", RF_cv
 clf_vc.fit(X_train, Y_train)
 # clf_vc.predict_proba(X_test) # For only soft voting
 pred_clf_cv = clf_cv.predict(X_test)
+
+##################################### LSTM - Classification ########################################
+
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten, LSTM
+from keras.layers import GlobalMaxPooling1D
+from keras.models import Model
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.text import Tokenizer
+from keras.layers import Input
+from keras.layers.merge import Concatenate
+from keras.layers import Bidirectional
+from keras.optimizers import SGD
+from keras.optimizers import Adam
+
+# Ensure that data is scaled
+scaler = MinMaxScaler(feature_range=(0, 1))
+df = scaler.fit_transform(df)
+
+# Dataframe (df) will be in this format (Person1, Target_Value, t1_feature1, t1_feature2 ; Person1, Target_Value, t2_feature1, t1_feature2)
+# Reshape into 3D array (sample, timesteps, n_features)
+X = df.drop([Target_Value], axis=1)
+X_3d = array(X).reshape(len(X_train[col].unique()), 10, len(X_train.columns)-1) # 10 timesteps, Col refers to unique identifier across sets of samples (e.g. Name) (-1 to remove "Person" ID from n_features)
+
+Y = df[[Person, Target_Value]].drop_duplicates()
+Y[Target_Value].value_counts()
+Y = Y[Target_Value]
+
+# OHE Target Variable (Y)
+from keras.utils.np_utils import to_categorical
+Y = Y.astype('category')
+Y = Y.cat.codes
+Y = to_categorical(Y, num_classes=2)
+
+# Train Test Split (80/20)
+X_train_3d, X_test_3d, Y_train, Y_test = train_test_split(X_3d, Y, shuffle=True, test_size=0.2)
+
+# Simple LSTM model
+model = Sequential()
+model.add(LSTM(150, activation='relu', return_sequences=True, input_shape=(10, 14))) #input_shape = (time_steps, n_features)
+model.add(LSTM(50, activation='relu'))
+model.add(Dropout(0.2)) # Add drop-out layer to prevent overfitting
+model.add(Dense(2, activation='softmax')) # https://keras.io/api/layers/activations/
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=1e-3), metrics=['accuracy']) #loss: https://keras.io/api/losses/
+
+model.fit(X_train_3d, Y_train, epochs=50, verbose=1, shuffle=False, batch_size=200, validation_data=(X_test_3d, Y_test))
+print(model.summary())
+
+# Feature Importance (SHAP)
+
+# Prediction
+pred_LSTM_train_scaled = model.predict(X_train_3d)
+pred_LSTM_test_scaled = model.predict(X_test_3d)
+# invert predictions
+pred_LSTM_train = scaler.inverse_transform(pred_LSTM_train_scaled)
+Actual_Y_train = scaler.inverse_transform([Y_train])
+pred_LSTM_test = scaler.inverse_transform(pred_LSTM_test_scaled)
+Actual_Y_test = scaler.inverse_transform([Y_test])
+
+# Note: There also exists the ability to retain memory between batches if needed.
+# https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
 
 ################################################# Classifier Evaluations ####################################################
 
@@ -242,6 +500,68 @@ model.compile(loss='mse', optimizer='adam')
 # fit the keras model on the dataset
 model.fit(X, y, epochs=10, batch_size=10)
 pred_NNR = model.predict(X_test)
+
+##################################### LSTM - Classification ########################################
+# Most predictions like Revenue etc. use timestep=1
+
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten, LSTM
+from keras.layers import GlobalMaxPooling1D
+from keras.models import Model
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.text import Tokenizer
+from keras.layers import Input
+from keras.layers.merge import Concatenate
+from keras.layers import Bidirectional
+from keras.optimizers import SGD
+from keras.optimizers import Adam
+
+# Ensure that data is scaled
+scaler = MinMaxScaler(feature_range=(0, 1))
+df = scaler.fit_transform(df)
+
+# Dataframe (df) will be in this format (Person1, Target_Value, t1_feature1, t1_feature2 ; Person1, Target_Value, t2_feature1, t1_feature2)
+# Reshape into 3D array (sample, timesteps, n_features)
+X = df.drop([Target_Value], axis=1)
+X_3d = array(X).reshape(len(X_train[col].unique()), timesteps, len(X_train.columns)-1) # 10 timesteps, Col refers to unique identifier across sets of samples (e.g. Name) (-1 to remove "Person" ID from n_features)
+
+Y = df[[Person, Target_Value]].drop_duplicates()
+Y = Y[Target_Value]
+
+# OHE Target Variable (Y)
+from keras.utils.np_utils import to_categorical
+Y = Y.astype('category')
+Y = Y.cat.codes
+Y = to_categorical(Y, num_classes=2)
+
+# Train Test Split (80/20)
+X_train_3d, X_test_3d, Y_train, Y_test = train_test_split(X_3d, Y, shuffle=True, test_size=0.2)
+
+# Simple LSTM model
+model = Sequential()
+model.add(LSTM(4, input_shape=(timesteps, look_back))) # lookback=1: X=t, y=t+1
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam') #loss: https://keras.io/api/losses/
+
+model.fit(X_train_3d, Y_train, epochs=50, verbose=1, shuffle=False, batch_size=200, validation_data=(X_test_3d, Y_test))
+print(model.summary())
+
+# Feature Importance (SHAP)
+
+# Prediction
+pred_LSTM_train_scaled = model.predict(X_train_3d)
+pred_LSTM_test_scaled = model.predict(X_test_3d)
+# invert predictions
+pred_LSTM_train = scaler.inverse_transform(pred_LSTM_train_scaled)
+Actual_Y_train = scaler.inverse_transform([Y_train])
+pred_LSTM_test = scaler.inverse_transform(pred_LSTM_test_scaled)
+Actual_Y_test = scaler.inverse_transform([Y_test])
+
+# Note: There also exists the ability to retain memory between batches if needed.
+# https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
 
 ################################################# Regression Evaluations ####################################################
 
